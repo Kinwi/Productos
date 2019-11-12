c1bbc4a77138884650d126155fd6f11500d65531
Raúl López Hernando <raullopezhernando@gmail.com>
2019-11-12 14:48:44 +0100

    Funcionando

--- Productos/Controllers/ProductosController.cs
+++ Productos/Controllers/ProductosController.cs
@@ -19,22 +19,31 @@ namespace Productos.Controllers
             new Producto { Id = 3, Nombre = "Ciruela", Descripcion = "Fruta proveniente del ciruelo", Tipo = "Fruta", Precio = 0.55, FechaCaducidad = Convert.ToDateTime("14/11/2019"), Imagen = "https://www.sportlifeargentina.com/media/cache/big/upload/images/article/5c57b1450ee6942e4d34a2f8/5c57b3bb0de694906e3494db-fruta-de-verano-ciruela-para-deportistas.jpg" },
             new Producto { Id = 4, Nombre = "Naranja", Descripcion = "Fruta proveniente del naranjo", Tipo = "Fruta", Precio = 0.29, FechaCaducidad = Convert.ToDateTime("17/11/2019"), Imagen = "https://static.elcorreo.com/www/multimedia/201902/08/media/cortadas/GF0N1J31-U401131236871ktC-U70573831277euB-624x385@El%20Correo-ElCorreo.jpg" },
             new Producto { Id = 5, Nombre = "Melocoton", Descripcion = "Fruta proveniente del melocotonero", Tipo = "Fruta", Precio = 1.3, FechaCaducidad = Convert.ToDateTime("13/11/2019"), Imagen = "https://www.hogarmania.com/archivos/201305/melocotones-xl-668x400x80xX.jpg" }
-           
 
-            };
+
+            
+           // return View(empleado.OrderBy(empleado => empleado.Apellido.ToList()));
+
+             };
         }
 
 
 
-        public IActionResult Index()
+        public IActionResult Index(string nombre)
         {
 
-            ViewData["productos"] = Productos.OrderBy(producto => producto.Nombre).ToList();
-            ViewBag.Nombre = "Arrate";
+            //ViewData["productos"] = Productos.OrderBy(producto => producto.Nombre).ToList();
+            //ViewBag.Nombre = "Arrate";
 
+            if (String.IsNullOrEmpty(nombre))
+            {
+                return View(Productos);
+            }
 
+            Productos = Productos.Where(x => x.Nombre.ToLower().Contains(nombre.ToLower())).ToList();
+            return View(Productos);
 
-                return View(Productos.OrderBy(producto => producto.Nombre).ToList());
+            //return View(Productos.OrderBy(producto => producto.Nombre).ToList());
             
         }
 
